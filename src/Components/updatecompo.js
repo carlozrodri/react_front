108472b9c34c3952d0bf59b0a11a072374154d20
carlozrodri <patica100@gmail.com>
2022-03-23 17:31:45 +0000

    fix

--- src/Components/updatecompo.js
+++ src/Components/updatecompo.js
@@ -1,116 +1,116 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import { Nav, Navbar, Card  } 
-from "react-bootstrap";
-import AsyncSelect from 'react-select/async';
-
-
-
+import { Nav, Navbar, Card } from "react-bootstrap";
+import AsyncSelect from "react-select/async";
 
 function UpdateCompo() {
-// navbar
-    const [inputValue, setValue] = useState('');
-    const [selectedValue, setSelectedValue] = useState('');
-    const [valornue, valorNuevo] = useState('');
-    const [nuevalista, setList2] = useState('');
-    const [newList, newList2] = useState('');
-  
-// updatecompo     
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [items, setItems] = useState([]);
-    
+  // navbar
+  const [inputValue, setValue] = useState("");
+  const [selectedValue, setSelectedValue] = useState("");
+  const [valornue, valorNuevo] = useState("");
+  const [nuevalista, setList2] = useState("");
+  const [newList, newList2] = useState("");
 
-    // Note: the empty deps array [] means
-    // this useEffect will run once
-    // similar to componentDidMount()
-    useEffect(() => {
+  // updatecompo
+
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+  useEffect(() => {
     fetch("https://amazfunels.herokuapp.com/api/")
-        .then(res => res.json())
-        .then(
+      .then((res) => res.json())
+      .then(
         (result) => {
-            setIsLoaded(true);
-            setItems(result);
+          setIsLoaded(true);
+          setItems(result);
         },
-        // Nota: es importante manejar errores aquí y no en 
+        // Nota: es importante manejar errores aquí y no en
         // un bloque catch() para que no interceptemos errores
         // de errores reales en los componentes.
         (error) => {
-            setIsLoaded(true);
-            setError(error);
+          setIsLoaded(true);
+          console.log(error)
         }
-        )
-    }, [])
-    // handle input change event
-  const handleInputChange = value => {
+      );
+  }, []);
+  // handle input change event
+  const handleInputChange = (value) => {
     valorNuevo(value);
     setValue(value);
   };
 
-// handle selection
-  const handleChange = value => {
+  // handle selection
+  const handleChange = (value) => {
     setSelectedValue(value);
     setList2(value);
-    
   };
-/// load options
-const loadOptions = (inputValue) => {
-
-    return fetch(`https://amazfunels.herokuapp.com/api/categorias/?search=${inputValue}`).then(res => res.json()).then();
+  /// load options
+  const loadOptions = (inputValue) => {
+    console.log(inputValue);
+    return fetch(
+      `https://amazfunels.herokuapp.com/api/categorias/?search=${inputValue}`
+    )
+      .then((res) => res.json())
+      .then();
   };
 
-
-// error catch
-    if (error) {
-    return <div>Error: {error.message}</div>;}
+  // error catch
 
 
+useEffect(()=>{
+const getSearch =()=>{
+     fetch(
+        `https://amazfunels.herokuapp.com/api/?search=${selectedValue.name}`
+      )
+        .then((res) => res.json())
+        .then((data) => {console.log(data);setItems(data)});
+}
+getSearch()
+},[selectedValue])
 
-// cargando
-     else if (!isLoaded) {
-    return <div>Loading...</div>;}
+  // cargando
+  if (!isLoaded) {
+    return <div><h1>Loading...</h1></div>;
+  } 
 
-    else if (selectedValue)  {
-        const loadOptions = fetch(`https://amazfunels.herokuapp.com/api/?search=${selectedValue.name}`).then(res => res.json()).then();
-          console.log(loadOptions)
-return<div><h1>{JSON.stringify(loadOptions.title)}</h1></div>
-
-    }
-
-
-// cargado    
-     else {
+  // cargado
+  else {
     return (
-  <div>
-           <AsyncSelect
-        defaultOptions
-        value={selectedValue}
-        getOptionLabel={e => e.name}
-        loadOptions={loadOptions}
-        onInputChange={handleInputChange}
-        onChange={handleChange}
-      />
+      <div>
+        <AsyncSelect
+          defaultOptions
+          value={selectedValue}
+          getOptionLabel={(e) => e.name}
+          loadOptions={loadOptions}
+          onInputChange={handleInputChange}
+          onChange={handleChange}
+        />
 
-      <pre>Selected Value1: {JSON.stringify(nuevalista.name || {}, null)}</pre>    
- 
+        <pre>
+          Selected Value1: {JSON.stringify(nuevalista.name || {}, null)}
+        </pre>
 
-    <div className='contenido'>
-    {items.map(todo =>
-<div key={todo.id} className='card'>
-  <Card.Img src={todo.item_pictures} />
-  <Card.Body>
-    <Card.Title>{todo.title} </Card.Title>
-    <Card.Text> {todo.item_description}</Card.Text>
-  </Card.Body>
-  <Card.Footer>
-    <small className="text-muted">Buy it NOW! on Amazon.co.uk</small>
-  </Card.Footer>
-     </div>
-     
-     )}
-    </div>
-</div>
+        <div className="contenido">
+          {items.map((todo) => (
+            <div key={todo.id} className="card">
+              <Card.Img src={todo.item_pictures} />
+              <Card.Body>
+                <Card.Title>{todo.title} </Card.Title>
+                <Card.Text> {todo.item_description}</Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <small className="text-muted">
+                  Buy it NOW! on Amazon.co.uk
+                </small>
+              </Card.Footer>
+            </div>
+          ))}
+        </div>
+      </div>
     );
-    }
+  }
 }
 export default UpdateCompo;
