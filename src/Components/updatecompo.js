74fbbe32a720af0415904d99c66575445cde541a
Carlos Rodriguez <patica100@gmail.com>
2022-03-29 17:03:24 +0100

    fix searchBar

--- src/Components/updatecompo.js
+++ src/Components/updatecompo.js
@@ -2,18 +2,14 @@ import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import { Card } from "react-bootstrap";
 import AsyncSelect from "react-select/async";
-import { BrowserRouter as Router, Routes,
-    Route, Link, NavLink } from "react-router-dom";
-  
+
 function UpdateCompo() {
   // navbar
-  const [error, setError] = useState(null);
-
-  const [inputValue, setValue] = useState("");
+  // const [inputValue, setValue] = useState("");
   const [selectedValue, setSelectedValue] = useState("");
   const [isLoaded, setIsLoaded] = useState(false);
   const [items, setItems] = useState([]);
-  const url = 'https://amazfunels.herokuapp.com/api/'
+  const url = "https://amazfunels.herokuapp.com/api/";
   // Note: the empty deps array [] means
   // this useEffect will run once
   // similar to componentDidMount()
@@ -22,26 +18,24 @@ function UpdateCompo() {
       .then((res) => res.json())
       .then(
         (result) => {
-          setItems(result);
           setIsLoaded(true);
-          
-          console.log(result);
+          setItems(result);
         },
         // Nota: es importante manejar errores aquí y no en
         // un bloque catch() para que no interceptemos errores
         // de errores reales en los componentes.
         (error) => {
           setIsLoaded(true);
-          setError(error);
+          console.log(error);
         }
       );
   }, []);
   // handle input change event
-  const handleInputChange = (value) => {
-    setValue(value);
-  };
+  // const handleInputChange = (value) => {
+  //   setValue(value);
+  //   console.log(value);
+  // };
 
-  
   // handle selection
   const handleChange = (value) => {
     setSelectedValue(value);
@@ -49,65 +43,58 @@ function UpdateCompo() {
   /// load options
   const loadOptions = (inputValue) => {
     console.log(inputValue);
-    return fetch(
-      `${url}categorias/?search=${inputValue}`
-    )
+    return fetch(`${url}categorias/?search=${inputValue}`)
       .then((res) => res.json())
       .then();
   };
 
   // error catch
 
-
-useEffect(()=>{
-const getSearch =()=>{
-     fetch(
-        `${url}?search=${selectedValue.name}`
-      )
-        .then((res) => res.json())
-        .then((data) => setItems(data));
-}
-getSearch()
-},[selectedValue])
+  useEffect(() => {
+    if (selectedValue) {
+      const getSearch = () => {
+        fetch(`${url}?search=${selectedValue.name}`)
+          .then((res) => res.json())
+          .then((data) => setItems(data));
+      };
+      getSearch();
+    }
+  }, [selectedValue]);
 
   // cargando
-  if (!items) {
-    return <div>items cargados</div>
+  if (!isLoaded) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
   }
-  else if (!isLoaded) {
-    return <div><h1>Loading...</h1></div>;
-  } 
 
   // cargado
   else {
-      
     return (
-        
       <div>
         <AsyncSelect
-        
           defaultOptions
-          placeholder='Search...'
+          placeholder="Search..."
           value={selectedValue}
           getOptionLabel={(e) => e.name}
           loadOptions={loadOptions}
-          onInputChange={handleInputChange}
+          // onInputChange={handleInputChange}
           onChange={handleChange}
         />
-
 
         <div className="contenido">
           {items.map((todo) => (
             <div key={todo.id} className="card">
-              <Card.Body >
-              <div>
-                <a href = {todo.url_amazon}>
-                <Card.Img src={todo.item_pictures}/>
-                </a>
-                </div> 
-                  
+              <Card.Body>
+                <div>
+                  <a href={todo.url_amazon}>
+                    <Card.Img src={todo.item_pictures} />
+                  </a>
+                </div>
                 <Card.Title>{todo.title} </Card.Title>
-                <Card.Text> {todo.item_description }</Card.Text>
+                <Card.Text> {todo.item_description}</Card.Text>
               </Card.Body>
               <Card.Footer>
                 <small className="text-muted">
