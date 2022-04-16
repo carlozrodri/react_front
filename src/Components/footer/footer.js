a3560dbc68da23afb8f4e2552383dd0d1279d4f7
Carlos Rodriguez <patica100@gmail.com>
2022-04-17 00:12:21 +0100

    footer

---
+++ src/Components/footer/footer.js
@@ -0,0 +1,85 @@
+import "./footer.css";
+
+function FooterBar() {
+    return ( 
+        
+        <div>
+<footer id="footer" className="footer-1">
+<div className="
+">
+<div className="container">
+<div className="row">
+  
+<div className="col-xs-12 col-sm-6 col-md-3">
+<div className="widget subscribe no-box">
+<h5 className="widget-title">COMPANY NAME<span></span></h5>
+<p>About the company, little discription will goes here.. </p>
+</div>
+</div>
+
+<div className="col-xs-12 col-sm-6 col-md-3">
+<div className="widget no-box">
+<h5 className="widget-title">Quick Links<span></span></h5>
+<ul className="thumbnail-widget">
+<li>
+<div className="thumb-content"><a href="#.">Get Started</a></div> 
+</li>
+<li>
+<div className="thumb-content"><a href="#.">Top Leaders</a></div> 
+</li>
+<li>
+<div className="thumb-content"><a href="#.">Success Stories</a></div> 
+</li>
+<li>
+<div className="thumb-content"><a href="#.">Event/Tickets</a></div> 
+</li>
+<li>
+<div className="thumb-content"><a href="#.">News</a></div>  
+</li>
+<li>
+<div className="thumb-content"><a href="#.">Lifestyle</a></div> 
+</li>
+<li>
+<div className="thumb-content"><a href="#.">About</a></div> 
+</li>
+</ul>
+</div>
+</div>
+
+<div className="col-xs-12 col-sm-6 col-md-3">
+<div className="widget no-box">
+<h5 className="widget-title">Get Started<span></span></h5>
+<p>Get access to your full Training and Marketing Suite.</p>
+<a className="btn" href="https://bit.ly/3m9avif" target="_blank">Subscribe Now</a>
+</div>
+</div>
+
+<div className="col-xs-12 col-sm-6 col-md-3">
+
+<div className="widget no-box">
+<h5 className="widget-title">Contact Us<span></span></h5>
+
+<p><a href="mailto:info@domain.com" title="glorythemes">info@domain.com</a></p>
+
+</div>
+</div>
+
+</div>
+</div>
+</div>
+  
+<div className="footer-copyright">
+<div className="container">
+<div className="row">
+<div className="col-md-12 text-center">
+<p>Copyright Company Name © 2021. All rights reserved.</p>
+</div>
+</div>
+</div>
+</div>
+</footer>
+      </div>
+ );
+}
+
+export default FooterBar;
