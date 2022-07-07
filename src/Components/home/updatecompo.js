ea8e8070cca2a48b40780ef2f44a255b8360facd
Carlos Rodriguez <patica100@gmail.com>
2022-07-07 16:48:56 +0100

    fix css

--- src/Components/home/updatecompo.js
+++ src/Components/home/updatecompo.js
@@ -1,33 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.css';
-import { Card } from 'react-bootstrap';
-import AsyncSelect from 'react-select/async';
-import { components } from 'react-select';
-import { motion } from 'framer-motion';
-import { customStyles } from './barstyles';
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import { Card } from "react-bootstrap";
+import AsyncSelect from "react-select/async";
+import { components } from "react-select";
+import { motion } from "framer-motion";
+import { customStyles } from "./barstyles";
 
 // import chroma from 'chroma-js';
 // import FetchData2 from "../data/data";
 //fortawesome icons
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearchengin, faAmazon } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearchengin, faAmazon } from "@fortawesome/free-brands-svg-icons";
 
 function UpdateCompo() {
-  const [selectedValue, setSelectedValue] = useState('');
+  const [selectedValue, setSelectedValue] = useState("");
   const [isLoaded, setIsLoaded] = useState(false);
   const [items, setItems] = useState([]);
   const history = useHistory();
-  const url = 'https://amazfunels.herokuapp.com/api/';
-
-
+  const url = "https://amazfunels.herokuapp.com/api/";
 
   useEffect(() => {
     let baseUrl;
-    let category = '';
+    let category = "";
     console.log(history);
-    if (history?.location?.pathname?.split('/')[1]) {
-      category = history?.location?.pathname?.split('/')[1];
+    if (history?.location?.pathname?.split("/")[1]) {
+      category = history?.location?.pathname?.split("/")[1];
       baseUrl = `${url}categorias/?search=${category}`;
     } else {
       baseUrl = url;
@@ -105,8 +103,6 @@ function UpdateCompo() {
   else {
     return (
       <div>
-
-
         <motion.div
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 3 }}
@@ -130,21 +126,28 @@ function UpdateCompo() {
         </motion.div>
         <div className="contenido">
           {items.map((todo) => (
-            <motion.a className="card" href={todo.url_amazon}   initial={{ opacity: 0 }}
+            <motion.a className="card"                 initial={{ opacity: 0 }}
                 whileInView={{ opacity: 3 }}
                 viewport={{ once: true }}
                 transition={{ duration: 0.3 }}
                 whileHover={{ scale: 1.1 }}
                 whileTap={{ scale: 0.9 }}
-                key={todo.id}>
+                key={todo.id} href={todo.url_amazon}>
+              <motion.div
+                className="card-container"
 
+              >
                 <Card.Body>
-                  <div className="contador">
-                    <p></p>
+                  <div className="body-carta">
+                    <div className="contador">
+                      <p></p>
+                    </div>
+                    <div className="card-contenido">
+                      <Card.Img style={{ backgroundImage: `url(${todo.item_pictures})`}}/>
+                      <Card.Text> {todo.item_description}</Card.Text>
+                    </div>
                   </div>
-                    <Card.Img src={todo.item_pictures} />     
-                  <Card.Text>{todo.title} </Card.Text>    
-                  <Card.Text> {todo.item_description}</Card.Text>
+                  {/* <Card.Text>{todo.title} </Card.Text>     */}
                 </Card.Body>
                 <Card.Footer>
                   <motion.button
@@ -160,10 +163,8 @@ function UpdateCompo() {
                   <FontAwesomeIcon icon={faAmazon} size="lg" />
                   <small className="text-muted">{"  "}Amazon.co.uk</small>
                 </Card.Footer>
-           
-              
+              </motion.div>
             </motion.a>
-            
           ))}
         </div>
       </div>
