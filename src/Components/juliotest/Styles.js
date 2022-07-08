0a4a309b8936d5d458470fb0a792c65dcddbd041
Carlos Rodriguez <patica100@gmail.com>
2022-07-08 23:15:52 +0100

    navbar styled

--- src/Components/juliotest/Styles.js
+++ src/Components/juliotest/Styles.js
@@ -2,30 +2,26 @@ import { motion } from "framer-motion";
 import styled from "styled-components";
 
 export const Header = styled.header`
-
-  background: green;
-  background: blue;
+  background: #d1e6ff;
   position: relative;
   right: 100;
   display: flex;
   padding: 1rem;
+  border-radius: 5rem;
   justify-content: flex-end;
   z-index: 2;
 `;
 
 export const Nav = styled(motion.nav)`
-  background-color: brown;
   height: 70vh;
   width: 100vw;
-
-  background-color: blue;
+  background-color: #a3cdff;
+  border-radius: 0.5rem;
   height: 40vh;
   width: 30vw;
-
   position: fixed;
   top: 0;
   z-index: 1;
-
   display: flex;
   flex-direction: column;
   justify-content: center;
