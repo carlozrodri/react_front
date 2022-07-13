8c1a419b053f6ccb9fcb857d0d3f1f0c7cbc76d8
carlozrodri <patica100@gmail.com>
2022-07-14 00:02:57 +0100

    css update

---
+++ src/Components/itemlist/Itemlist.jsx
@@ -0,0 +1,58 @@
+import "./itemlist.css";
+import { motion } from "framer-motion";
+
+function ItemList() {
+  return (
+    <motion.div class="list-items">
+      <div class="row">
+
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 3 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.3 }}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          class="col"
+        >
+          Sportwear
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 3 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.3 }}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          class="col"
+        >
+          Garden
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 3 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.3 }}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          class="col"
+        >
+          Summer
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 3 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.3 }}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          class="col"
+        >
+          Sportwear
+        </motion.div>
+      </div>
+    </motion.div>
+  );
+}
+
+export default ItemList;
