09d58f2593749c689cced847b01ec176e38aeee7
carlozrodri <patica100@gmail.com>
2022-07-26 23:39:10 +0100

    cookies cosent added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,10 +4,13 @@ import UpdateCompo from "../Components/home/updatecompo";
 import EmailBar from "../Components/emailbar/emailbar";
 import FooterBar from "../Components/footer/footer";
 import ItemList from "../Components/itemlist/Itemlist";
+import CookieConsent from "react-cookie-consent";
 
 const Home = () => {
   return (
     <Fragment>
+      <CookieConsent>This website uses cookies to enhance the user experience.</CookieConsent>
+
       <ItemList />
       <NavBar />
       <UpdateCompo />
