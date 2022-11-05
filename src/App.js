f31f8e9a905d05a6e3324f4f15305e13728b5292
carlozrodri <patica100@gmail.com>
2022-11-05 17:32:28 +0000

    testing vercel analytic

--- src/App.js
+++ src/App.js
@@ -7,10 +7,14 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Contactame from "./pages/Contactame/Contactame";
 import About from "./pages/About/About";
+import { inject } from '@vercel/analytics';
+
 export default function App() {
+  inject();
   return (
     <Router>
       <div>
+        
         <Switch>
           <Route path = "/about" children={<About />} />
           <Route path='/contactame' children={<Contactame />} />
